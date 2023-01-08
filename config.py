26d7a5a1d8b8d486b41f25e31a3f378c5f49e0b2
Donko Dimov <donko@abv.bg>
2023-01-08 19:01:01 +0100

    fix: Rem vars

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ load_dotenv(dotenv_path=env_path)
 
 class Settings:
 
-    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
-    DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    #POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    #POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    #POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    #POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
+    #POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    #DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     AUTH0_DOMAIN : str = os.getenv("AUTH0_DOMAIN")
     AUTH0_CLIENT_ID : str = os.getenv("AUTH0_CLIENT_ID")
