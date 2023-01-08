66027edc5e3112d503d6148dc81c461b06796652
Donko Dimov <donko@abv.bg>
2023-01-08 18:54:08 +0100

    fix: Add correct db address

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ export ALGORITHMS=['RS256']
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
-export DATABASE_URL="postgresql://postgres@localhost:5432/heroku_local"
+export DATABASE_URL="postgres://rmdtscbaxkixsv:3e7ec1fb9a11bea8b9e6e82a09c940a87bd0269f19961c621b363139ba144d9d@ec2-34-207-12-160.compute-1.amazonaws.com:5432/daem1ptu1m4r7h"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
